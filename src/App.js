52f04d09012c47186e7c76a425bf1c88423be369
Zarina1708 <zarinadgkllgefhj@gmail.com>
2022-12-16 23:09:15 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  state = {
+      count: 0,
+  };
+
+  incrementCount = () => {
+      this.setState({
+          count: this.state.count + 1,
+      });
+  };
+
+  render() {
+      return (
+          <div>
+          <h1> Нажми на кнопку !</h1>
+          <button onClick={this.incrementCount}>Count: {this.state.count}</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
